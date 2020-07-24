1761a6a4c8d69bc39b4b3b19ef2a1e8c789797fb
Asaf Peer <asafpr@gmail.com>
2020-07-24 05:23:25 -0400

    add double time fix week

--- plot_hosp_change.R
+++ plot_hosp_change.R
@@ -9,4 +9,10 @@ ggplot(hosp, aes(date, mod_sev)) + geom_point() + geom_smooth(method = "loess", 
 ggplot(hosp, aes(date, change_rate)) + geom_point() + geom_smooth(method = "loess", formula = "y ~ x") + theme_bw()
 hosp$week <- week(hosp$date)
 weekly <- group_by(hosp, week) %>% summarise_all(mean) %>% ungroup()
+weekly <- weekly %>% filter(week<max(week)) # remove the last week which is incomplete
+weekly$change_rate <- c(NA, (weekly$mod_sev[2:nrow(weekly)] / weekly$mod_sev[1:(nrow(weekly)-1)] - 1 ) * 100)
 ggplot(weekly, aes(date, mod_sev))+ geom_point() + geom_smooth(method = "loess", formula = "y ~ x", span=0.3) + theme_bw()
+ggplot(weekly, aes(date, change_rate)) + geom_point() + geom_smooth(method = "loess", formula = "y ~ x") + theme_bw()
+ggplot(hosp, aes(date, mod_sev)) + geom_point() + geom_smooth(method = "loess", formula = "y ~ x", span=0.3) + theme_bw() + scale_y_log10() + scale_x_date(limits = as.Date(c("2020-06-15","2020-07-19")))
+hosp$dbl_time <- 100*log(2)/hosp$change_rate
+ggplot(hosp, aes(date, dbl_time)) + geom_point() + scale_x_date(limits = as.Date(c("2020-06-15","2020-07-19"))) +  theme_bw() + ylim(0,50)
