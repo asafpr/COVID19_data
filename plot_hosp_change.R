35318944d1c043e0c1a9703f005dcba2023a1170
Asaf Peer <asafpr@gmail.com>
2020-07-24 03:01:58 -0400

    Update plot_hosp_change.R

--- plot_hosp_change.R
+++ plot_hosp_change.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 hosp <- read_csv("corona_hospitalization_ver_0027.csv", na = c("", "NA", "<15", "NULL"))
 colnames(hosp) <- c("date", "hospitalized", "F_perc_hosp", "mean_age_hosp", "std_age_hosp", "ventilators", "F_perc_vent", "mean_age_vent", "std_age_vent", "mild_cases", "F_perc_mild", "mean_age_mild", "std_age_mild", "moderate_cases", "F_perc_moderate", "mean_age_moderate", "std_age_moderate", "severe_cases", "F_perc_severe", "mean_age_severe", "std_age_severe")
 hosp$mod_sev <- hosp$moderate_cases + hosp$severe_cases
