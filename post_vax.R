7be90d35a155e03bf4a55f005b727d47d196b9cc
Asaf Peer <asafpr@gmail.com>
2021-01-12 17:35:22 -0500

    update brewer

--- post_vax.R
+++ post_vax.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(RColorBrewer)
 pres <- data.frame(age_group = c("16-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70-79", "80-89", "90+"),
                    week_1_tests = c(1315, 6474, 10353, 14507, 15247, 16270, 9323, 5955, 3123),
                    week_1_positive_rate = c(6.4, 6.1, 4.2, 3.8, 4.6, 5.1, 4.9, 4.5, 3.3),
@@ -13,6 +14,9 @@ pres <- data.frame(age_group = c("16-19", "20-29", "30-39", "40-49", "50-59", "6
 pres$week_1_positive <- round(pres$week_1_tests * pres$week_1_positive_rate / 100)
 pres$week_2_positive <- round(pres$week_2_tests * pres$week_2_positive_rate / 100)
 pres$week_3_positive <- round(pres$week_3_tests * pres$week_3_positive_rate / 100)
+pres$`Week 1 hosp ratio` <- pres$week_1_hospitalized / pres$week_1_positive
+pres$`Week 2 hosp ratio` <- pres$week_2_hospitalized / pres$week_2_positive
+pres$`Week 3 hosp ratio` <- pres$week_3_hospitalized / pres$week_3_positive
 
 
 pl <- pres %>% filter(age>35) %>% select(age_group, ends_with("positive"), ends_with("zed")) %>% pivot_longer(cols = -age_group, names_to = "category", values_to = "number")
@@ -32,5 +36,5 @@ pml %>% ggplot(aes(age_group, p, color=week)) +
   geom_errorbar(aes(ymin = ci_lo, ymax = ci_hi, width=0.1)) +
   geom_point(size=3) + ggtitle("Hospitalized individuals post Pfizer BNT162b2 1st dose in Israel") + 
   ylab("Hospitalized/Positives fraction") + 
-  xlab("Age group") + scale_color_discrete(labels = c("Within 1st week", "Within 2nd week", "Within 3rd week"), name = "Positive result") +
+  xlab("Age group") + scale_color_brewer(palette = "Dark2", labels = c("Within 1st week", "Within 2nd week", "Within 3rd week"), name = "Positive result") +
   theme_bw() 
